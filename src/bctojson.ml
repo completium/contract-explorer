8aad7c41c4d13a0ee0f0c86a6d43bd2d7a3e57c8
Guillaume Duhamel <guillaume.duhamel@edukera.com>
2019-12-02 19:18:56 +0100

    add Arg usage for bctojson

--- src/bctojson.ml
+++ src/bctojson.ml
@@ -2,6 +2,41 @@ open Yojson
 open Safe
 open Util
 
+(* Options ------------------------------------------------------------------*)
+module type Options = sig
+  val getContractId : unit -> string
+  val setContractId : string -> unit
+  val getAddress    : unit -> string
+  val setAddress    : string -> unit
+  val getPort       : unit -> string
+  val setPort       : string -> unit
+  val getBranch     : unit -> string
+  val setBranch     : string -> unit
+  val getPath       : unit -> string
+  val setPath       : string -> unit
+end
+module MOptions : Options = struct
+  let contractId = ref "KT1BoLiscRVVgBkvFSRrbDTJkDfpzCDkBKa2"
+  let getContractId _ = !contractId
+  let setContractId s = contractId := s
+
+  let address = ref "localhost"
+  let getAddress _ = !address
+  let setAddress s = address := s
+
+  let port = ref "8732"
+  let getPort _ = !port
+  let setPort s = port := s
+
+  let branch = ref "main"
+  let getBranch _ = !branch
+  let setBranch s = branch := s
+
+  let path = ref "."
+  let getPath _ = !path
+  let setPath s = path := s
+end
+
 (* Tools --------------------------------------------------------------------*)
 
 module type RPC = sig
@@ -12,11 +47,11 @@ end
 module Rpc : RPC = struct
 
   exception RPCNotFound of string * (Curly.Error.t)
-  
+
   let url_to_string url =
     match Curly.(run (Request.make ~url:url ~meth:`GET ())) with
-      | Ok x -> x.Curly.Response.body
-      | Error e -> raise (RPCNotFound(url,e))
+    | Ok x -> x.Curly.Response.body
+    | Error e -> raise (RPCNotFound(url,e))
 
   let url_to_json url = Safe.from_string (url_to_string url)
 
@@ -64,22 +99,22 @@ type contract = {
 }
 [@@deriving yojson, show {with_path = false}]
 
-let cmp_contracts c1 c2 = 
-(c1.storage = c2.storage) && 
-(c1.balance = c2.balance) 
+let cmp_contracts c1 c2 =
+  (c1.storage = c2.storage) &&
+  (c1.balance = c2.balance)
 
 (* fabrics ------------------------------------------------------------------*)
 
 module type Url = sig
   val getBlock    : string -> string
-  val getHead     : unit -> string
+  val getHead     : unit   -> string
   val getContract : string -> string -> string
 end
 
 module type Block = sig
   val mk_id   : string -> block_id
   val mk_data : string -> block_data
-end 
+end
 
 module type Contract = sig
   val mk : string -> string -> string -> contract option
@@ -96,39 +131,39 @@ module type Writer = sig
   val write_op : op -> unit
 end
 
-module Make_Writer (Dirs : sig 
-  val path : string
-  val contract : string
-end) : Writer = struct
+module Make_Writer (Dirs : sig
+    val path : string
+    val contract : string
+  end) : Writer = struct
   let out_contracts = ref stdout
   let out_ops = ref stdout
   let out_contract_info = ref stdout
 
-  let open_logs () = 
+  let open_logs () =
     out_contracts := open_out_gen [Open_creat;Open_append] 0o640 (Dirs.path^"/"^Dirs.contract^"/contracts.json");
     out_ops := open_out_gen [Open_creat;Open_append] 0o640 (Dirs.path^"/"^Dirs.contract^"/ops.json");
     out_contract_info := open_out_gen [Open_creat;Open_append] 0o640 (Dirs.path^"/"^Dirs.contract^"/contract_info.json")
 
-  let close_logs () = 
+  let close_logs () =
     close_out !out_contracts;
     close_out !out_ops;
     close_out !out_contract_info
 
-  let print_json channel json = 
+  let print_json channel json =
     let str = Yojson.Safe.to_string json in
     Printf.fprintf channel "%s" (str^"\n");
     flush channel
 
-  let write_contract contract = 
+  let write_contract contract =
     print_json !out_contracts (contract_to_yojson contract)
 
-  let write_op op = 
+  let write_op op =
     print_json !out_ops (op_to_yojson op)
 
-  let write_contract_info st = 
+  let write_contract_info st =
     print_json !out_contract_info (contract_info_to_yojson st)
-  
-end 
+
+end
 
 (* Tezos --------------------------------------------------------------------*)
 
@@ -138,35 +173,35 @@ module Make_TzURL (Info : BCinfo) : Url = struct
   let getContract b c = (getBlock b)^"/context/contracts/"^c
 end
 
-module Make_TzBlock (Url : Url) (Rpc : RPC) : Block = struct 
+module Make_TzBlock (Url : Url) (Rpc : RPC) : Block = struct
 
-  let mk_id hash = 
+  let mk_id hash =
     let json = Rpc.url_to_json (Url.getBlock hash) in {
       hash         = hash;
       previous     = json |> member "header" |> member "predecessor" |> to_string;
     }
 
-  let json_to_ops timestamp json : op list = 
-    json |> member "operations" |> to_list |> List.fold_left (fun acc ops -> 
-      ops |> to_list |> List.fold_left (fun acc op ->
-        let hash = op |> member "hash" |> to_string in 
-        op |> member "contents" |> to_list |> List.fold_left (fun acc c ->
-          let kind = c |> member "kind" |> to_string in
-          match kind with
-          | "transaction" -> acc @ [{
-            hash = hash;
-            timestamp = timestamp;
-            source = c |> member "source" |> to_string;
-            destination = c |> member "destination" |> to_string;
-            parameters = c |> member "parameters" |> Safe.to_string;
-          }]
-          | _ -> acc
-        ) acc
-      ) acc
-    ) []
+  let json_to_ops timestamp json : op list =
+    json |> member "operations" |> to_list |> List.fold_left (fun acc ops ->
+        ops |> to_list |> List.fold_left (fun acc op ->
+            let hash = op |> member "hash" |> to_string in
+            op |> member "contents" |> to_list |> List.fold_left (fun acc c ->
+                let kind = c |> member "kind" |> to_string in
+                match kind with
+                | "transaction" -> acc @ [{
+                    hash = hash;
+                    timestamp = timestamp;
+                    source = c |> member "source" |> to_string;
+                    destination = c |> member "destination" |> to_string;
+                    parameters = c |> member "parameters" |> Safe.to_string;
+                  }]
+                | _ -> acc
+              ) acc
+          ) acc
+      ) []
 
-  let mk_data hash = 
-    let json = Rpc.url_to_json (Url.getBlock hash) in 
+  let mk_data hash =
+    let json = Rpc.url_to_json (Url.getBlock hash) in
     let timestamp = json |> member "header" |> member "timestamp" |> to_string in {
       timestamp = timestamp;
       operations = json_to_ops timestamp json;
@@ -176,28 +211,28 @@ end
 
 module Make_TzContract (Url : Url) (Block : Block) (Rpc : RPC) : Contract = struct
 
-  let mk timestamp bhash chash = 
-    try 
+  let mk timestamp bhash chash =
+    try
       let json = Rpc.url_to_json (Url.getContract bhash chash) in (Some {
-        timestamp = timestamp;
-        storage = json |> member "script" |> member "storage" |> Safe.to_string;
-        balance = json |> member "balance" |> to_string;
-      })
+          timestamp = timestamp;
+          storage = json |> member "script" |> member "storage" |> Safe.to_string;
+          balance = json |> member "balance" |> to_string;
+        })
     with _ -> None
 
-    let json_to_storage_type json = 
-      json |> member "script" |> member "code" |> to_list |> List.fold_left (fun acc code ->
+  let json_to_storage_type json =
+    json |> member "script" |> member "code" |> to_list |> List.fold_left (fun acc code ->
         let prim = code |> member "prim" |> to_string in
         if compare prim "storage" = 0 then
           code |> member "args" |> to_list |> fun l -> List.nth l 0 |> Safe.to_string
         else acc
       ) ""
 
-    let mk_data chash = 
-      let json = Rpc.url_to_json (Url.getContract "head" chash) in {
-        storage_type = json_to_storage_type json;
-        entries = [];
-      }
+  let mk_data chash =
+    let json = Rpc.url_to_json (Url.getContract "head" chash) in {
+      storage_type = json_to_storage_type json;
+      entries = [];
+    }
 
 end
 
@@ -210,9 +245,9 @@ module Make_ContractExplorer (Block : Block) (Contract : Contract) (Writer : Wri
     (* dump operations *)
     let data = Block.mk_data block.hash in
     let ops = List.filter(fun op ->
-      let { hash=_; source=src; destination=dst } = op in 
+        let { hash=_; source=src; destination=dst } = op in
         compare src contract_key = 0 || compare dst contract_key = 0
-    ) data.operations in
+      ) data.operations in
     if List.length ops > 0 then
       List.iter (fun op -> Writer.write_op op) ops;
     print_string ("."); flush stdout;
@@ -223,27 +258,31 @@ module Make_ContractExplorer (Block : Block) (Contract : Contract) (Writer : Wri
           Writer.write_contract previous_contract;
         Writer.write_contract c;
         explore false contract_key c block)
-      else 
+      else
         explore first contract_key c block
     | None -> (
-      print_endline "\nNO MORE CONTRACT";
-      if first then
-        Writer.write_contract previous_contract
-    )
-    
+        print_endline "\nNO MORE CONTRACT";
+        if first then
+          Writer.write_contract previous_contract
+      )
+
 end
 
-let main () =
-  let contract_key = "KT1BoLiscRVVgBkvFSRrbDTJkDfpzCDkBKa2" in
-  let module TzInfo : BCinfo = struct 
-    let getIp () = "localhost"
-    let getPort () = "8732"
-    let getBranch () = "main"
+let process contract_key =
+  let contract_key = match contract_key with
+    | "" -> Format.eprintf "no contract"; exit 1
+    | _  -> contract_key
+  in
+  (* let  = "KT1BoLiscRVVgBkvFSRrbDTJkDfpzCDkBKa2" in *)
+  let module TzInfo : BCinfo = struct
+    let getIp () = MOptions.getAddress()
+    let getPort () = MOptions.getPort()
+    let getBranch () = MOptions.getBranch()
   end in
-  let module Writer = Make_Writer (struct 
-    let path = "/Users/benoitrognier/Documents/contract-explorer"
-    let contract = contract_key
-  end) in 
+  let module Writer = Make_Writer (struct
+      let path = "."
+      let contract = contract_key
+    end) in
   Writer.open_logs();
   let module Url = Make_TzURL (TzInfo) in
   let module Block = Make_TzBlock (Url) (Rpc) in
@@ -251,11 +290,27 @@ let main () =
   let module Explorer = Make_ContractExplorer (Block) (Contract) (Writer) in
   let init_block = "head" in
   match Contract.mk "" init_block contract_key with
-  | Some c -> 
+  | Some c ->
     let cinfo = Contract.mk_data contract_key in
     Writer.write_contract_info cinfo;
     Explorer.explore true contract_key c { hash=""; previous=init_block }
   | _ -> print_endline ("Contract not found in "^init_block);
-  Writer.close_logs()
+    Writer.close_logs()
+
+let main () =
+  let print_version () = print_endline "0.1"; exit 0 in
+  let arg_list = Arg.align [
+      "--address", Arg.String (MOptions.setAddress), "<address> Set address";
+      "--branch", Arg.String (MOptions.setBranch), "<branch> Set branch";
+      "--port", Arg.String (MOptions.setPort), "<port> Set port";
+      "-v", Arg.Unit (fun () -> print_version ()), " Show version number and exit";
+      "--version", Arg.Unit (fun () -> print_version ()), " Same as -v";
+    ] in
+  let arg_usage = String.concat "\n" [
+      "usage : bctojson [OPTIONS] <contract_id>";
+      "";
+      "Available options:";
+    ] in
+  Arg.parse arg_list process arg_usage
 
 let _ = main ()
