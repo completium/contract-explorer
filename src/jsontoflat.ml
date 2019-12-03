a84b214ce285a029b140c7261228109bc6825f66
Benoît Rognier <benoit.rognier@edulera.com>
2019-12-03 11:44:21 +0100

    adds dumper

--- src/jsontoflat.ml
+++ src/jsontoflat.ml
@@ -4,7 +4,7 @@ open Util
 
 let storage = "{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[{\"int\":\"808\"},{\"prim\":\"False\"}]},{\"prim\":\"Pair\",\"args\":[{\"bytes\":\"ab\"},{\"int\":\"1\"}]}]},{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[[{\"int\":\"20\"},{\"int\":\"21\"}],[{\"prim\":\"Elt\",\"args\":[{\"int\":\"30\"},{\"int\":\"30\"}]},{\"prim\":\"Elt\",\"args\":[{\"int\":\"31\"},{\"int\":\"31\"}]},{\"prim\":\"Elt\",\"args\":[{\"int\":\"32\"},{\"int\":\"32\"}]}]]},{\"prim\":\"Pair\",\"args\":[[{\"prim\":\"Elt\",\"args\":[{\"int\":\"40\"},[{\"int\":\"400\"},{\"int\":\"401\"},{\"int\":\"402\"}]]},{\"prim\":\"Elt\",\"args\":[{\"int\":\"50\"},[{\"int\":\"500\"},{\"int\":\"501\"},{\"int\":\"502\"}]]}],[{\"prim\":\"Elt\",\"args\":[{\"int\":\"60\"},[{\"prim\":\"Elt\",\"args\":[{\"int\":\"70\"},{\"int\":\"80\"}]},{\"prim\":\"Elt\",\"args\":[{\"int\":\"71\"},{\"int\":\"81\"}]}]]},{\"prim\":\"Elt\",\"args\":[{\"int\":\"61\"},[{\"prim\":\"Elt\",\"args\":[{\"int\":\"77\"},{\"int\":\"88\"}]},{\"prim\":\"Elt\",\"args\":[{\"int\":\"99\"},{\"int\":\"101\"}]}]]}]]}]}]},{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[[{\"prim\":\"Elt\",\"args\":[{\"int\":\"1001\"},{\"prim\":\"Pair\",\"args\":[{\"int\":\"1002\"},{\"string\":\"myval0\"}]}]},{\"prim\":\"Elt\",\"args\":[{\"int\":\"1003\"},{\"prim\":\"Pair\",\"args\":[{\"int\":\"1004\"},{\"string\":\"myval1\"}]}]}],{\"int\":\"2\"}]},{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Some\",\"args\":[{\"int\":\"3\"}]},[{\"int\":\"777\"},{\"int\":\"888\"},{\"int\":\"999\"}]]}]},{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[{\"string\":\"mystr\"},{\"prim\":\"Unit\"}]},{\"prim\":\"Pair\",\"args\":[{\"int\":\"4\"},{\"string\":\"myval\"}]}]}]}]}"
 let storage_type = "{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"big_map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"int\"}],\"annots\":[\"%mbmap\"]},{\"prim\":\"bool\",\"annots\":[\"%mbool\"]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"bytes\",\"annots\":[\"%mbytes\"]},{\"prim\":\"int\",\"annots\":[\"%mint\"]}]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"list\",\"args\":[{\"prim\":\"int\"}],\"annots\":[\"%mlist\"]},{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"int\"}],\"annots\":[\"%mmap\"]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"list\",\"args\":[{\"prim\":\"int\"}]}],\"annots\":[\"%mmaplist\"]},{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"int\"}]}],\"annots\":[\"%mmapmap\"]}]}]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"pair\",\"args\":[{\"prim\":\"int\",\"annots\":[\"%id\"]},{\"prim\":\"string\",\"annots\":[\"%value\"]}]}],\"annots\":[\"%mmapmyrecord\"]},{\"prim\":\"nat\",\"annots\":[\"%mnat\"]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"option\",\"args\":[{\"prim\":\"int\"}],\"annots\":[\"%moption\"]},{\"prim\":\"set\",\"args\":[{\"prim\":\"int\"}],\"annots\":[\"%mset\"]}]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"string\",\"annots\":[\"%mstring\"]},{\"prim\":\"unit\",\"annots\":[\"%munit\"]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"int\",\"annots\":[\"%id\"]},{\"prim\":\"string\",\"annots\":[\"%value\"]}],\"annots\":[\"%myr\"]}]}]}]}"
-
+let storage_type2 = "{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"big_map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"int\"}]},{\"prim\":\"bool\"}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"bytes\"},{\"prim\":\"int\"}]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"list\",\"args\":[{\"prim\":\"int\"}]},{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"int\"}]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"list\",\"args\":[{\"prim\":\"int\"}]}]},{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"int\"}]}]}]}]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"map\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"pair\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"string\"}]}]},{\"prim\":\"nat\"}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"option\",\"args\":[{\"prim\":\"int\"}]},{\"prim\":\"set\",\"args\":[{\"prim\":\"int\"}]}]}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"pair\",\"args\":[{\"prim\":\"string\"},{\"prim\":\"unit\"}]},{\"prim\":\"pair\",\"args\":[{\"prim\":\"int\"},{\"prim\":\"string\"}]}]}]}]}"
 (* Michelson value ----------------------------------------------------------*)
 
 type ordered =
@@ -46,8 +46,35 @@ type mtype =
 | Tlist of mtype
 | Tmap of ordered_mtype * mtype
 | Tset of ordered_mtype
+[@@deriving yojson, show {with_path = false}]
 and amtype = string option * mtype
 [@@deriving yojson, show {with_path = false}]
+
+let rec fold_amtype acc (amt : amtype) =
+match amt with
+| None,Tpair (t1,t2) -> fold_amtype (fold_amtype acc t1) t2
+| _ as t -> acc @ [t]
+
+let rec mtype_to_string = function
+| Tpair (a1,a2) -> "PAIR of ("^(amtype_to_string a1)^") and ("^(amtype_to_string a2)^")"
+| Tordered Tint -> "INT"
+| Tordered Tnat -> "NAT"
+| Tordered Tstr -> "STR"
+| Tordered Tbytes -> "BYTES"
+| Toption t -> "OPTION of ("^(mtype_to_string t)^")"
+| Tbool -> "BOOL"
+| Tunit -> "UNIT"
+| Tlist t -> "LIST of ("^(mtype_to_string t)^")"
+| Tmap (t1,t2) -> "MAP of ("^(mtype_to_string (Tordered t1))^") to ("^(mtype_to_string t2)^")"
+| Tset t -> "SET of ("^(mtype_to_string (Tordered t))^")"
+| _ as t -> Format.printf "%a" pp_mtype t;raise Not_found
+and amtype_to_string amt =
+let types = fold_amtype [] amt in
+(String.concat "\n" (List.mapi (fun i t ->
+    match t with
+    | Some l,t -> l^" : "^(mtype_to_string t)
+    | None, t -> "field"^(string_of_int i)^" : "^(mtype_to_string t)
+) types))
 
 (* flat storage type --------------------------------------------------------*)
 
@@ -96,7 +123,10 @@ let amtype_to_ordered : amtype -> ordered_mtype = function
 
 let get_annot keys json =
 if List.mem "annots" keys then
-    Some (json |> member "annots" |> to_list |> fun l -> List.nth l 0 |> to_string)
+    Some (json  |> member "annots" |> to_list
+                |> fun l -> List.nth l 0
+                |> to_string
+                |> fun s -> String.sub s 1 (String.length s - 1))
 else None
 
 let rec json_to_mtype (json : Safe.t) : amtype =
@@ -106,29 +136,24 @@ let rec json_to_mtype (json : Safe.t) : amtype =
         match prim with
         | "pair" -> begin
             match json |> member "args" |> to_list with
-            | arg1 :: arg2 :: [] ->
-                (None, Tpair (json_to_mtype arg1, json_to_mtype arg2))
+            | arg1 :: arg2 :: [] -> (get_annot keys json, Tpair (json_to_mtype arg1, json_to_mtype arg2))
             | _ -> raise (ExpectedNbargs 2) end
         | "big_map" | "map" -> begin
             match json |> member "args" |> to_list with
             | arg1 :: arg2 :: [] ->
-                (get_annot keys json,
-                Tmap (
+                (get_annot keys json, Tmap (
                     amtype_to_ordered (json_to_mtype arg1),
                     snd (json_to_mtype arg2)))
             | _ -> raise (ExpectedNbargs 2) end
         | "list" -> begin
             match json |> member "args" |> to_list with
-            | arg :: [] ->
-                (get_annot keys json, Tlist (snd (json_to_mtype arg)))
+            | arg :: [] -> (get_annot keys json, Tlist (snd (json_to_mtype arg)))
             | _ -> raise (ExpectedNbargs 1) end
         | "set" -> begin
             match json |> member "args" |> to_list with
-            | arg :: [] ->
-                (get_annot keys json, Tset (amtype_to_ordered (json_to_mtype arg)))
+            | arg :: [] -> (get_annot keys json, Tset (amtype_to_ordered (json_to_mtype arg)))
             | _ -> raise (ExpectedNbargs 1) end
-        | "bool" ->
-            (get_annot keys json, Tbool)
+        | "bool" -> (get_annot keys json, Tbool)
         | "int" -> (get_annot keys json, Tordered Tint)
         | "nat" -> (get_annot keys json, Tordered Tnat)
         | "string" -> (get_annot keys json, Tordered Tstr)
@@ -143,7 +168,9 @@ let rec json_to_mtype (json : Safe.t) : amtype =
 
 let rec json_to_mvalue json : mvalue =
     print_endline (Safe.to_string json);
-    try let _l = to_list json in Munit
+    try
+        let _l = to_list json in
+        Munit
     with _ ->
     let keys= json |> keys in
     if List.mem "prim" keys then
@@ -177,12 +204,16 @@ let rec json_to_mvalue json : mvalue =
 let main () =
   let storage = Safe.from_string storage in
   let storage_type = Safe.from_string storage_type in
-  print_endline (Safe.to_string storage);
-  print_endline "";
-  Format.printf "%a" pp_mvalue (json_to_mvalue storage);
+  let storage_type2 = Safe.from_string storage_type2 in
+ (*  print_endline (Safe.to_string storage_type);
   print_endline "";
   Format.printf "%a" pp_amtype (json_to_mtype storage_type);
+  print_endline ""; *)
+  print_endline (amtype_to_string (json_to_mtype storage_type));
   print_endline "";
-  print_endline (Safe.to_string storage_type)
+  print_endline (amtype_to_string (json_to_mtype storage_type2));
+  print_endline "";
+  Format.printf "%a" pp_mvalue (json_to_mvalue storage);
+  print_endline ""
 
 let _ = main ()
